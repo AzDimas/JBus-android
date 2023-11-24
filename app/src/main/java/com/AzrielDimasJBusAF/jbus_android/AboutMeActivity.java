92b4f03d8f52234a33ee93d6f4b82a3e38146a1b
unknown <dimasazriel45@gmail.com>
2023-11-24 15:30:30 +0700

    CS09 - Menyelesaikan Case Study

--- app/src/main/java/com/AzrielDimasJBusAF/jbus_android/AboutMeActivity.java
+++ app/src/main/java/com/AzrielDimasJBusAF/jbus_android/AboutMeActivity.java
@@ -1,26 +1,86 @@
 package com.AzrielDimasJBusAF.jbus_android;
 
+import static com.AzrielDimasJBusAF.jbus_android.LoginActivity.loggedAccount;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.AzrielDimasJBusAF.jbus_android.model.BaseResponse;
+import com.AzrielDimasJBusAF.jbus_android.request.BaseApiService;
+import com.AzrielDimasJBusAF.jbus_android.request.UtilsApi;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AboutMeActivity extends AppCompatActivity {
 
     private TextView isiDariEmail;
     private TextView isiDariUser;
     private TextView isiDariBalance;
+    private Context mContext;
+    private BaseApiService mApiService;
+    private EditText topupAmount;
+    private Button topupButton = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_me);
+
+        mContext = this;
+        mApiService = UtilsApi.getApiService();
         isiDariUser = findViewById(R.id.username);
         isiDariEmail = findViewById(R.id.email);
         isiDariBalance = findViewById(R.id.balance);
 
-        isiDariUser.setText("azriel");
-        isiDariEmail.setText("azriel@gmail.com");
-        isiDariBalance.setText("Rp999999999");
+        isiDariUser.setText(loggedAccount.name);
+        isiDariEmail.setText(loggedAccount.email);
+        Double strdouble = new Double(loggedAccount.balance);
+        isiDariBalance.setText(strdouble.toString());
+
+        mContext = this;
+        mApiService = UtilsApi.getApiService();
+        topupAmount = findViewById(R.id.topup);
+        topupButton = findViewById(R.id.ButtonTopUp);
+        topupButton.setOnClickListener(v -> handleTopup());
+        }
+
+    protected void handleTopup(){
+        String topUpAmountS = topupAmount.getText().toString();
+        if (topUpAmountS.isEmpty()) {
+            Toast.makeText(mContext, "Field cannot be empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        double topUpD = Double.valueOf(topUpAmountS);
+        mApiService.topUp(LoginActivity.loggedAccount.id, topUpD).enqueue(new Callback<BaseResponse<Double>>() {
+            @Override
+            public void onResponse(Call<BaseResponse<Double>> call, Response<BaseResponse<Double>> response) {
+                // handle the potential 4xx & 5xx error
+                if (!response.isSuccessful()) {
+                    Toast.makeText(mContext, "Application error " + response.code(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                BaseResponse<Double> res = response.body();
+                // if success finish this activity
+                if (res.success){
+                    finish();
+                    LoginActivity.loggedAccount.balance += res.payload;
+                    startActivity(getIntent());
+                    Toast.makeText(mContext, res.message, Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(mContext, res.message, Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onFailure(Call<BaseResponse<Double>> call, Throwable t) {
+                Toast.makeText(mContext, "Problem with the server", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/AzrielDimasJBusAF/jbus_android/LoginActivity.java
+++ app/src/main/java/com/AzrielDimasJBusAF/jbus_android/LoginActivity.java
@@ -6,26 +6,42 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.AzrielDimasJBusAF.jbus_android.model.Account;
+import com.AzrielDimasJBusAF.jbus_android.model.BaseResponse;
+import com.AzrielDimasJBusAF.jbus_android.request.BaseApiService;
+import com.AzrielDimasJBusAF.jbus_android.request.UtilsApi;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextView registerNow = null;
     private Button loginButton = null;
+    private BaseApiService mApiService;
+    private Context mContext;
+    private EditText email, password;
+    public static Account loggedAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mContext = this;
+        mApiService = UtilsApi.getApiService();
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(v -> handleLogin());
 
         registerNow = findViewById(R.id.register_now);
-        loginButton = findViewById(R.id.login_button);
-        registerNow.setOnClickListener(v -> {
+        registerNow.setOnClickListener( v ->{
             moveActivity(this, RegisterActivity.class);
-        });
-        loginButton.setOnClickListener(v -> {
-            moveActivity(this, MainActivity.class);
         });
         getSupportActionBar().hide();
     }
@@ -37,5 +53,40 @@ public class LoginActivity extends AppCompatActivity {
 
     private void viewToast(Context ctx, String message){
         Toast.makeText(ctx, message, Toast.LENGTH_SHORT).show();
+    }
+
+    protected void handleLogin() {
+        String emailS = email.getText().toString();
+        String passwordS = password.getText().toString();
+        if (emailS.isEmpty() || passwordS.isEmpty()) {
+            Toast.makeText(mContext, "Field cannot be empty",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mApiService.login(emailS, passwordS).enqueue(new Callback<BaseResponse<Account>>() {
+            @Override
+            public void onResponse(Call<BaseResponse<Account>> call, Response<BaseResponse<Account>> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(mContext, "Application error " + response.code(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                BaseResponse<Account> res = response.body();
+
+                if (res.success) {
+                    finish();
+                    viewToast(mContext, "Welcome To JBus");
+                    loggedAccount = res.payload;
+                    Toast.makeText(mContext, res.message, Toast.LENGTH_SHORT).show();
+                    moveActivity(mContext, MainActivity.class);
+                }
+                else{
+                    Toast.makeText(mContext, "Check your email and password", Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onFailure(Call<BaseResponse<Account>> call, Throwable t) {
+                Toast.makeText(mContext, "Problem with the server", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
